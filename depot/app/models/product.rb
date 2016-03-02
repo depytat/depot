196d50b350255b38c49259677f9ba4b4908219ac
depytat <ziabko4488@gmail.com>
2016-03-02 10:47:16 +0300

    second

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :title , :description , :image_url, presence: true
+	validates :price , numericality: {greater_than _or_equal_to : 0.01}
+	validates :title , uniqueness: true
+
+	validates :image_url, allow_blank: true , format: {
+		with : %r{\.(gif|jpg|png)$}i,
+		message: 'URL должен указывать на изображение формата GIF, 3PG или PNG.'
+	}
 end
